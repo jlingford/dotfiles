5ddbc789a763f315610c5ee439ac3727b4fe5965
JamesLingford <james.lingford@gmail.com>
2022-11-08 22:16:43 +1100

    updated fishrc to open fishrc with an alias

--- .config/omf/key_bindings.fish
+++ .config/omf/key_bindings.fish
@@ -9,8 +9,8 @@ alias bat='bat --style changes,header,grid,snip,numbers --line-range :20'
 alias v='nvim'
 
 # Moving to favourite folders/files
-alias vimrc='cd ; cd ~/.config/nvim'
-alias bashrc='cd ; cd ~/.config/omf'
+alias vimrc='cd ; cd ~/.config/nvim ; ls -l' 
+alias bashrc='cd ; v ~/.config/omf/key_bindings.fish'
 
 # Fuzzy finder aliases with fzf
 # se() { du -a ~/.config/* | awk '{print $2}' | fzf | xargs -r $EDITOR ;}
